80374cb66b3024843f3a8e9f5547d3d8a6c4cbee
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-05-02 16:54:00 +0500

    search api in progress need individual search panel

---
+++ search-api/src/components/PoliceList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class PoliceList extends Component{
+
+    state = {
+        loading: true,
+        
+    }
+    render(){
+
+        return(
+            <div></div>
+        )
+    }
+}
