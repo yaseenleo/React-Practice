f1e966b278aa70c50a962425e606d1e0a2641172
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-05-16 13:00:21 +0500

    project started

---
+++ fbpost-library/src/component/Post.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class Post extends Component{
+
+constructor(){
+    super()
+    this.state={
+        profile: "",
+        
+    }
+}
+
+
+    render(){
+
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
