5da064636f8c56889194cd9527dffa8c5785fb88
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-09-25 23:05:19 +0500

    implimentation of global instances example

--- axios-practice/src/App.js
+++ axios-practice/src/App.js
@@ -1,7 +1,9 @@
 import Title from "./components/Title";
 // import Setup from "./examples/1-first-request";
 // import Setup from "./examples/2-headers";
-import Setup from "./examples/3-post-request";
+// import Setup from "./examples/3-post-request";
+import Setup from "./examples/4-global-instance";
+import "./axios/global";
 
 function App() {
   return (
--- axios-practice/src/examples/4-global-instance.js
+++ axios-practice/src/examples/4-global-instance.js
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 
-const productsUrl = 'https://course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = "https://course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const res1 = await axios(productsUrl);
+      const res2 = await axios(randomUserUrl);
+      console.log(res1);
+      console.log(res2);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
