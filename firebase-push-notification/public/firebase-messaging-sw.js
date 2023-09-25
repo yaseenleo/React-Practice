e48ebb1dc41dcb044223540e55a964dae1766f0e
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-09-25 15:26:34 +0500

    firebase script version update

--- firebase-push-notification/public/firebase-messaging-sw.js
+++ firebase-push-notification/public/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging.js");
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxbcsYBpN-hv98SvWw1lOkKzDEOQYaExQ",
