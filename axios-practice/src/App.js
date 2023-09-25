0fc882964610096283ac39cf456c3994418f6484
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-09-25 16:55:02 +0500

    post request implimentation example

--- axios-practice/src/App.js
+++ axios-practice/src/App.js
@@ -1,6 +1,7 @@
 import Title from "./components/Title";
 // import Setup from "./examples/1-first-request";
-import Setup from "./examples/2-headers";
+// import Setup from "./examples/2-headers";
+import Setup from "./examples/3-post-request";
 
 function App() {
   return (
