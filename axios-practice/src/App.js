226a7fcbb6f99e41f4dbac6255fde534ed3ea4a5
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-09-25 16:26:24 +0500

    axios get request example

---
+++ axios-practice/src/App.js
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+import Setup from "./examples/1-first-request";
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
