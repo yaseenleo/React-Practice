3733b4b38c5ccd3c222d07b259a9ac6829779c11
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-09-28 15:58:29 +0500

    custom instance example implimentation

--- axios-practice/src/App.js
+++ axios-practice/src/App.js
@@ -2,8 +2,10 @@ import Title from "./components/Title";
 // import Setup from "./examples/1-first-request";
 // import Setup from "./examples/2-headers";
 // import Setup from "./examples/3-post-request";
-import Setup from "./examples/4-global-instance";
-import "./axios/global";
+// import Setup from "./examples/4-global-instance";
+// import "./axios/global";
+
+import Setup from "./examples/5-custom-instance";
 
 function App() {
   return (
