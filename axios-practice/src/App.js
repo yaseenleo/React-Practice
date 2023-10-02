48d69028c56c3941e9d43a23433c667f5c42599e
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2023-10-02 15:09:08 +0500

    intercepter example code

--- axios-practice/src/App.js
+++ axios-practice/src/App.js
@@ -5,7 +5,8 @@ import Title from "./components/Title";
 // import Setup from "./examples/4-global-instance";
 // import "./axios/global";
 
-import Setup from "./examples/5-custom-instance";
+// import Setup from "./examples/5-custom-instance";
+import Setup from "./examples/6-interceptors";
 
 function App() {
   return (
